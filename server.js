8fe64b504770b6d2eafd93282c35b4375e82bd1f
jujaryu <jujaryu@gmail.com>
2018-08-01 14:00:39 +0900

    actionsOn

---
+++ server.js
@@ -0,0 +1,12 @@
+//=========================================================
+// Import NPM modules
+//=========================================================
+const express = require("express");
+const actionsOnGoogleAdapter = require("bot-framework-actions-on-google");
+
+const app = express();
+
+app.use(actionsOnGoogleAdapter("PyobJbySYMU.cwA.X28.noEzQHXj9dnTMD5jD9vF0BaetnxJ3dRyj7Rr4tL6kUM").router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`ActionsOnGoogle demo listening on port ${PORT}!`));
